bfc3d1631131475d66efdf95dec81b9209eadef6
Yuri Kim(iMac) <gamtoggi@gmail.com>
2019-01-17 21:58:14 +0900

    고객 추가 폼 작업중

--- customers/urls.py
+++ customers/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 app_name = 'customers'
 urlpatterns = [
-    # /customers/
+    # GET /customers
     path('', views.CustomerListView.as_view(), name='index'),
+
+    # /customers/create
+    path('create', views.CustomerCreateView.as_view(), name='create'),
+
 ]
--- customers/views.py
+++ customers/views.py
@@ -1,4 +1,7 @@
+from django.http import JsonResponse
+from django.template.loader import render_to_string
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin, AccessMixin
 
 from .models import Customer
@@ -8,3 +11,13 @@ class CustomerListView(LoginRequiredMixin, ListView):
     model = Customer
     paginate_by = 100
     template_name = 'customers/index.html'
+
+
+class CustomerCreateView(LoginRequiredMixin, CreateView):
+    model = Customer
+    fields = ['name', 'phone', 'kakao', 'address1', 'address2', 'birthday', 'memo']
+    template_name = 'customers/create_form.html'
+
+    def get(self, request):
+        html = render_to_string(self.template_name, {}, request=request)
+        return JsonResponse({'html': html})
