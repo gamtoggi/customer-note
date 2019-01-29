f9f13f332d2e422a52c8bbd729bf2713824b6c88
yuri kim <gamtoggi@gmail.com>
2019-01-30 01:37:47 +0900

    / 로 들어오면 /customers 로 redirect

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic.base import TemplateView
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', RedirectView.as_view(pattern_name='customers:list'), name='home'),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
